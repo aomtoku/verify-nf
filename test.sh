0aeca51b8ba9f1aa1ba727f39d6bb8260bd1f435
Yuta Tokusashi <aom@sfc.wide.ad.jp>
2020-08-10 14:57:41 +0100

    fixed

--- test.sh
+++ test.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-ssh -A 192.168.122.2 -l netfpga 'if [ ! -d NetFPGA-SUME-dev ]; then git clone git@github.com:NetFPGA/NetFPGA-SUME-dev.git fi'
-ssh -A 192.168.122.2 -l netfpga 'cd /home/netfpga/NetFPGA-SUME-dev/lib/sw/std/driver/sume_riffa_v1_0_0 && make && sudo make install && reboot'
+ssh -A 192.168.122.2 -l netfpga 'if [ ! -d NetFPGA-SUME-dev ]; then git clone git@github.com:NetFPGA/NetFPGA-SUME-dev.git; fi'
+ssh -A 192.168.122.2 -l netfpga 'cd /home/netfpga/NetFPGA-SUME-dev/lib/sw/std/driver/sume_riffa_v1_0_0 && make && sudo make install && sudo reboot'
 
 #sleep 10
